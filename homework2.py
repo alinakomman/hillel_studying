3a119be00eea5e8eb1dc217700b5f0bd4b3fc534
Alina <alinakomandina@Alinas-MacBook-Pro.local>
2023-07-19 20:51:36 +0200

    first commit

---
+++ homework2.py
@@ -0,0 +1,10 @@
+# 1
+word = input("Enter a word: ")
+string = f"Word '{word}' has {len(word)} letters"
+print(string)
+
+# 2
+second_word = input("Enter a word: ")
+word_length = len(word)
+second_string = f"Word '{second_word}' has {word_length} letters"
+print(second_string)
