f54d734d3c5d0f5b90ba43be36a0f7bfefd5bf0d
alinakomman <140008996+alinakomman@users.noreply.github.com>
2023-08-03 13:54:59 +0200

    Update homework_5.py

--- homework_5.py
+++ homework_5.py
@@ -3,20 +3,25 @@ def supply(weapons,amount):
     dead_russians = weapons * amount
     return dead_russians
 
+
 calculation = supply(70000000, 2)
 print(calculation)
+
 
 # b
 def first_gaslo(a,b):
     gaslo = a + b
     return gaslo
 
+
 say_it = first_gaslo('Slava', ' Ukraini!')
 print(say_it)
+
 
 # c
 def second_gaslo(*args):
     return args
+
 
 say_it = second_gaslo('Heroyam', ' Slava!')
 print(say_it)
